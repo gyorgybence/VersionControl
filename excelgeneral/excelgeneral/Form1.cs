b3b943ef038d6bfdb0764e158d94a6aeff655c00
Bence György <gyorgybence08@gmail.com>
2020-10-04 20:33:50 +0200

    7es8elejefeladat

--- excelgeneral/excelgeneral/Form1.cs
+++ excelgeneral/excelgeneral/Form1.cs
@@ -17,9 +17,9 @@ namespace excelgeneral
     {
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> Flats;
-        Excel.Application xlApp;
-        Excel.Workbook xlWB;
-        Excel.Worksheet xlSheet;
+        Excel.Application xlApp; // A Microsoft Excel alkalmazás
+        Excel.Workbook xlWB;  // A létrehozott munkafüzet
+        Excel.Worksheet xlSheet; // Munkalap a munkafüzeten belül
         public Form1()
         {
             InitializeComponent();
@@ -31,18 +31,24 @@ namespace excelgeneral
         {
             try
             {
+                // Excel elindítása és az applikáció objektum betöltése
                 xlApp = new Excel.Application();
+                //Új munkafüzet
                 xlWB = xlApp.Workbooks.Add(Missing.Value);
+                // Új munkalap
                 xlSheet = xlWB.ActiveSheet;
+                // Tábla létrehozása
                 CreateTable();
+                // Control átadása a felhasználónak
                 xlApp.Visible = true;
                 xlApp.UserControl = true;
             }
-            catch (Exception ex)
+            catch (Exception ex) // Hibakezelés a beépített hibaüzenettel
             {
 
                 string errMsg= string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
                 MessageBox.Show(errMsg, "Error");
+                // Hiba esetén az Excel applikáció bezárása automatikusan
                 xlWB.Close(false, Type.Missing, Type.Missing);
                 xlApp.Quit();
                 xlWB = null;
@@ -52,8 +58,91 @@ namespace excelgeneral
 
         private void CreateTable()
         {
-            throw new NotImplementedException();
+            string[] headers = new string[]
+            {
+                    "Kód",
+                    "Eladó",
+                    "Oldal",
+                    "Kerület",
+                    "Lift",
+                    "Szobák száma",
+                    "Alapterület (m2)",
+                    "Ár (mFt)",
+                    "Négyzetméter ár (Ft/m2)"
+            };
+            for (int i = 0; i < headers.Length; i++)
+            {
+                xlSheet.Cells[1,i+1] = headers[i];
+            }                    
+             object[,] values = new object[Flats.Count, headers.Length]; //ocject típusú két dimenziós tömb létrehozása az adatok tárolására
+             //  A tömb feltöltése adatokkal
+             int counter = 0;
+             foreach (Flat f in Flats)
+             {
+                values[counter, 0] = f.Code;
+                values[counter, 1] = f.Vendor;
+                values[counter, 2] = f.Side;
+                values[counter, 3] = f.District;
+                if (f.Elevator)
+                {
+                    values[counter, 4] = "VAN";
+                }
+                else
+                {
+                    values[counter, 4] = "NINCS";
+                }
+                
+                values[counter, 5] = f.NumberOfRooms;
+                values[counter, 6] = f.FloorArea;
+                values[counter, 7] = f.Price;
+                values[counter, 8] = "="+GetCell(counter+2,7)+"/"+GetCell(counter+2,6);
+                 counter++;
+                
+            }
+            xlSheet.get_Range(
+            GetCell(2, 1),
+            GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+
+            Excel.Range headerRange = xlSheet.get_Range(GetCell(1, 1), GetCell(1, headers.Length));
+            headerRange.Font.Bold = true;
+            headerRange.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+            headerRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            headerRange.EntireColumn.AutoFit();
+            headerRange.RowHeight = 40;
+            headerRange.Interior.Color = Color.LightBlue;
+            headerRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+
+
+
+
+
+
+
+
+
         }
+        // Excel koordináták meghatározása
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
+
+        }
+
+
+
+
 
         private void LoadData()
         {
