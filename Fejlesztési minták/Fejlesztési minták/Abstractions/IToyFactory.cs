8c4a64318e4892e489c935aae13f46c5091fe922
Bence György <gyorgybence08@gmail.com>
2020-11-15 21:35:40 +0100

    8

---
+++ Fejlesztési minták/Fejlesztési minták/Abstractions/IToyFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fejlesztési_minták.Abstractions
+{
+   public interface IToyFactory
+    {
+        Toy Createnew();
+    }
+}
