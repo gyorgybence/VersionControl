24b2778da45588cdedfd89bb7f31e404a46b909f
Bence György <gyorgybence08@gmail.com>
2020-11-15 22:24:29 +0100

    12

--- Fejlesztési minták/Fejlesztési minták/Form1.cs
+++ Fejlesztési minták/Fejlesztési minták/Form1.cs
@@ -77,5 +77,16 @@ namespace Fejlesztési_minták
         {
             Factory = new BallFactory();
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            var colorPicker = new ColorDialog();
+
+            colorPicker.Color = button.BackColor;
+            if (colorPicker.ShowDialog() != DialogResult.OK)
+                return;
+            button.BackColor = colorPicker.Color;
+        }
     }
 }
