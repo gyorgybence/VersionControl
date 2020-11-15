f27eca6fc7f0900391c6373a702a1102eda64dc1
Bence György <gyorgybence08@gmail.com>
2020-11-15 21:31:39 +0100

    7

---
+++ Fejlesztési minták/Fejlesztési minták/Entities/Car.cs
@@ -0,0 +1,19 @@
+﻿using Fejlesztési_minták.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fejlesztési_minták.Entities
+{
+    public class Car : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            Image imageFile = Image.FromFile("Images/car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+        }
+    }
+}
