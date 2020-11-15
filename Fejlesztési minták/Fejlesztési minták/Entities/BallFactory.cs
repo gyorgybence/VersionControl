6844da1778a04032513a91118ef4a7bc9982e617
Bence György <gyorgybence08@gmail.com>
2020-11-15 19:49:31 +0100

    3

---
+++ Fejlesztési minták/Fejlesztési minták/Entities/BallFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fejlesztési_minták.Entities
+{
+    class BallFactory
+    {
+        public Ball CreateNew()
+        {
+
+            return new Ball();
+        }
+    }
+}
