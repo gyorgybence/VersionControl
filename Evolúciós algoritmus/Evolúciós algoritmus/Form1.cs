12d2000ee1f078399108e850541d608f9fd45b8c
Bence György <gyorgybence08@gmail.com>
2020-11-29 19:58:08 +0100

    2.3.4

--- Evolúciós algoritmus/Evolúciós algoritmus/Form1.cs
+++ Evolúciós algoritmus/Evolúciós algoritmus/Form1.cs
@@ -7,14 +7,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WorldsHardestGame;
 
 namespace Evolúciós_algoritmus
 {
     public partial class Form1 : Form
     {
+        GameController gc = new GameController();
+        GameArea ga;
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
         public Form1()
         {
             InitializeComponent();
+            ga = gc.ActivateDisplay();
+            this.Controls.Add(ga);
+            
+            for (int i = 0; i < populationSize + 1; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
+
+            gc.Start();
         }
     }
 }
